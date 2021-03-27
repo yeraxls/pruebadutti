c3dfc4015f26cedf4a7f0d0f5ca2ea80050be7c9
yeraxls <yerayls91@gmail.com>
2021-03-27 15:30:59 +0100

    pruebas unitarias
    Pruebas unitarias en register y login

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -2,15 +2,29 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
+import { GestionUsuarioService } from 'src/app/services/gestion-usuario.service';
+import { ShipsComponent } from '../ships/ships.component';
 
 
 import { LoginComponent } from './login.component';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let router: Router;
   
+  const serviceMock ={
+    respuesta : false,
+    iniciarSesion:function(){ return  this.respuesta}
+    
+  }
+
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
@@ -18,7 +32,10 @@ describe('LoginComponent', () => {
         FormsModule, 
         RouterTestingModule.withRoutes([]),
         ReactiveFormsModule],
-        
+        providers:[
+        {provide: GestionUsuarioService, useValue: serviceMock},
+        { provide: Router, useValue: mockRouter }
+      ]
 
     })
     .compileComponents();
@@ -26,12 +43,36 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-   
+   router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Inicia sesión fallido', () => {
+    serviceMock.respuesta = true;
+    component.loginForm.setValue({ username: "Yeray", password: "aaaaaaa"});
+    component.loginUser();
+    expect(component.unregistered).toBe(true);
+  });
+
+  it('Inicia sesión correcto', () => {
+    component.loginForm.setValue({ username: "Yeray", password: "aaaaaaa"});
+    component.loginUser();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/principal/ships'])
+    expect(component.unregistered).toBe(false);
+  });
+  it('Es invalido', () => {
+    component.loginForm.setValue({ username: "a", password: "aaaaaaa"});
+    var valido = component.esInvalido;
+    expect(valido).toBe(false);
+  });
+  it('Es invalido', () => {
+    component.loginForm.setValue({ username: "aaaaa", password: "aaaaaaa"});
+    var valido = component.esInvalido;
+    expect(valido).toBe(true);
   });
 });
--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -2,22 +2,30 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GestionUsuarioService } from 'src/app/services/gestion-usuario.service';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  const serviceMock = {
+    respuesta: false,
+    registrarusuario: function (userLogin) { }
+
+  }
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ],
-      imports:[         
-        FormsModule, 
+      declarations: [RegisterComponent],
+      imports: [
+        FormsModule,
         RouterTestingModule.withRoutes([]),
         ReactiveFormsModule],
-    
+      providers: [
+        { provide: GestionUsuarioService, useValue: serviceMock },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -28,5 +36,41 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('invalido', () => {
+    component.registerForm.setValue(
+      {
+        first_name: "aaaaaa",
+        last_name: "aaaaaa",
+        username: "aa",
+        email: "aaaaaa",
+        password: "aaaaaa"
+      });
+    var invalido = component.esInvalido;
+    expect(invalido).toBe(true);
+  });
+  it('valido', () => {
+    component.registerForm.setValue(
+      {
+        first_name: "aaaaaa",
+        last_name: "aaaaaa",
+        username: "aaaaaaaa",
+        email: "aaaaaa",
+        password: "aaaaaa"
+      });
+    var invalido = component.esInvalido;
+    expect(invalido).toBe(false);
+  });
+
+  it('registrarse', () => {
+    component.registerForm.setValue(
+      {
+        first_name: "aaaaaa",
+        last_name: "aaaaaa",
+        username: "aaaaaa",
+        email: "aaaaaa",
+        password: "aaaaaa"
+      });
+    component.registerUser();
   });
 });
